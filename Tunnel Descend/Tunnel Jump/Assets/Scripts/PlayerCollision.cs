b5b66cff169c070f3a3f3a4dfb26c08081e9cdb1
Boekemeyer <tboekemeyer@hotmail.de>
2017-04-25 16:01:37 +0200

    explosions cause crash

---
+++ Tunnel Descend/Tunnel Jump/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public Transform Explosion;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Obstacle")
+        {
+            Instantiate(Explosion, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+            StaticHolder.PlayerDied = true;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Tunnel Descend/Tunnel Jump/Assets/Scripts/StaticHolder.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticHolder : MonoBehaviour
+{
+    public static bool PlayerDied = false;
+}
